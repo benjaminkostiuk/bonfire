94338f3225166cf72d17ef4f6dc76deddafa70b7
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-11-27 19:42:57 -0500

    Add function to format process names

--- bonfire/utils.py
+++ bonfire/utils.py
@@ -1,9 +1,15 @@
 import os
+import re
 
-'''
-Build an absolute path from a relative path in the bonfire package directory
-'''
+# Build an absolute path from a relative path in the bonfire package directory
 def build_abs_path(rel_path):
     current_dir = os.path.dirname(os.path.realpath(__file__))
     abs_path = os.path.join(current_dir, rel_path)
     return abs_path
+
+# Remove the .exe at the end of a process name and capitalize
+def format_process_name(process_name):
+    exp = re.compile('^(.*)\.exe$')
+    if exp.match(process_name):
+        process_name = exp.match(process_name).group(1)
+    return process_name.capitalize()
