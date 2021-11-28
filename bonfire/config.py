152953cd311ca703e1005cadaf92b87afeb46e99
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-11-27 22:08:52 -0500

    Update config with more apps and max shown apps

--- bonfire/config.py
+++ bonfire/config.py
@@ -8,15 +8,16 @@ class ApplicationOptions():
 
 # Model configuration information from config.json
 class Config:
-    def __init__(self, db_path, whitelist):
+    def __init__(self, db_path, whitelist, max_apps_in_list):
         self.db = TinyDB(build_abs_path(db_path))
         self.whitelist = whitelist
+        self.max_apps_in_list = max_apps_in_list
 
 # Open config.json and read contents
 PATH_TO_CONFIG = build_abs_path('../store/config.json')
 with open(PATH_TO_CONFIG, 'r') as config_file:
     config_opts = json.loads(config_file.read())
-configuration = Config(config_opts['db-path'], config_opts['apps']['whitelist'])
+configuration = Config(config_opts['db-path'], config_opts['apps']['whitelist'], config_opts['max-apps-shown'])
 
 # Return configuration object
 def config():
