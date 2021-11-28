738b212868da4d83fc3dca14cd8410baf4158477
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-11-27 19:43:22 -0500

    Update fire object to include process names

--- bonfire/fire.py
+++ bonfire/fire.py
@@ -1,27 +1,32 @@
 from datetime import datetime
+from .utils import format_process_name
 
 # Models a lit bonfire
 class Fire():
-    def __init__(self, processes_saved, id=None, timestamp=datetime.now(), nickname=None):
+    def __init__(self, apps, id=None, timestamp=datetime.now(), nickname=None):
         self.id = id
         self.timestamp = timestamp
-        self.processes_saved = processes_saved
+        self.apps = apps
         self.nickname = nickname
 
-    # Overloaded constructor to build a fire from a dict
+    # Class constructor to build a fire from a dict
     @classmethod
     def fromdict(cls, datadict):
         timestamp = datetime.strptime(datadict['timestamp'], '%m/%d/%Y %H:%M:%S')
-        return cls(datadict['processes_saved'], id=datadict.doc_id, timestamp=timestamp,nickname=datadict['nickname'])
+        return cls(datadict['apps'], id=datadict.doc_id, timestamp=timestamp, nickname=datadict['nickname'])
 
     # Get the list of attribute names for a bonfire
     @classmethod
     def attrs(cls):
-        return ['id', 'nickname', 'timestamp']
+        return ['Id', 'Timestamp', 'Nickname', 'Apps']
 
     # Return a list with attributes about the bonfire
     def rattrs(self):
-        return [self.id, self.nickname, self.timestamp]
+        MAX_APPS_SHOWN = 2
+        app_names = ', '.join(list(map(format_process_name, self.apps.keys()))[:MAX_APPS_SHOWN])
+        if len(self.apps) > MAX_APPS_SHOWN:
+            app_names += ' (+{} more)'.format(len(self.apps) - MAX_APPS_SHOWN)
+        return [self.id, self.timestamp, self.nickname, app_names]
         
     def __iter__(self):
         for key in self.__dict__:
