5f02af12f09d73f81faa969d71c43a0f38a4d53a
Benjamin Kostiuk <kostiukb@mcmaster.ca>
2021-11-27 20:21:06 -0500

    Add bonfire exceptions

---
+++ bonfire/exceptions.py
@@ -0,0 +1,8 @@
+
+# Occurs when multiple bonfires are found instead of 1
+class MultipleBonfiresFound(Exception):
+    pass
+
+# Occurs when no bonfires are found
+class BonfireNotFound(Exception):
+    pass
